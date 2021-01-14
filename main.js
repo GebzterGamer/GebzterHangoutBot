2b65c65479e518f54db2a9a6cd21aff1bdd01c74
GebzterGamer <72647746+GebzterGamer@users.noreply.github.com>
2021-01-14 16:23:34 +0000

    bot code

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const botsettings = require('./botsettings.json');
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online`)
+    bot.user.setActivity("Geb's Videos", {type: "WATCHING"});
+})
+
+
+
+bot.login(process.env.token);
